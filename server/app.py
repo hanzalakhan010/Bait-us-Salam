457e6d459ba3a82cc618bbc10faa397bfb6428e2
Hanzala Khan <pythondeveloper360@gmail.com>
2025-05-08 15:22:51 +0500

    add SQLAlchemy integration and Students model to Flask application

--- server/app.py
+++ server/app.py
@@ -1,7 +1,20 @@
 from Flask import flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:pos-72270@localhost/bwt"
+db = SQLAlchemy(app)
+
+class Students(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    
+
+
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
 
 if __name__ == "__main__":
