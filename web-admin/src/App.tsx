b188da81c189cc7106f45ae651987ea6ff56c68e
Hanzala Khan <pythondeveloper360@gmail.com>
2025-05-09 09:22:38 +0500

    add lucide-react icons and implement navigation buttons in App component

--- web-admin/src/App.tsx
+++ web-admin/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
+import { BookOpenText, GraduationCap, LayoutDashboard, Users } from 'lucide-react'
 import Student from './components/Student';
 
 function App() {
@@ -11,6 +12,25 @@ function App() {
     <>
       {login ?
         <>
+          <nav>
+            <button className="flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors">
+              <LayoutDashboard className="w-5 h-5" />
+              <span>Dashboard</span>
+            </button>
+            <button className="flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors">
+              <Users className="w-10 h-10" />
+              <span>Students</span>
+            </button>
+            <button className="flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors">
+            <BookOpenText className="w-10 h-10" />
+              <span>Courses</span>
+            </button>
+            <button className="flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors">
+            <GraduationCap className="w-20 h-5" />
+              <span>Instructors</span>
+            </button>
+            
+          </nav>
           {tab == 'dashboard' ? <Dashboard /> : null}
           {tab == 'students' ? <Student /> : null}
         </>
