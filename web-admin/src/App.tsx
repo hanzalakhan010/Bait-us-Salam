90eae5da609b47e2fb0c0f743aa6ba6cfd7b60df
Hanzala Khan <pythondeveloper360@gmail.com>
2025-05-12 04:39:34 +0500

    feat: implement navigation with React Router and update styling for consistency

--- web-admin/src/App.tsx
+++ web-admin/src/App.tsx
@@ -1,47 +1,62 @@
 import { useState } from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
-import Login from './components/Login';
 import { BookOpenText, FileText, GraduationCap, LayoutDashboard, Users } from 'lucide-react'
-import Student from './components/Student';
+import Students from './components/Students';
+import { Link } from 'react-router-dom';
+import NewStudent from './components/Students/newStudent';
 
 function App() {
-  const [login, setLogin] = useState(false)
   const [tab, setTab] = useState('dashboard')
   return (
     <>
-      {login ?
-        <>
-          <nav>
-            <button onClick={()=>setTab('dashboard')} className={`flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors ${tab == 'dashboard' ? 'active' : null}`}>
+
+      <>
+        <nav>
+          <div>
+            <Link to='/dasboard' className={`flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors ${tab == 'dashboard' ? 'active' : null}`}>
               <LayoutDashboard className="w-5 h-5" />
               <span>Dashboard</span>
-            </button>
-            <button onClick={()=>setTab('students')} className={`flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors ${tab == 'students' ? 'active' : null} `}>
+            </Link>
+          </div>
+          <div>
+            <Link to='/students' className={`flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors ${tab == 'students' ? 'active' : null} `}>
               <Users className="w-10 h-10" />
               <span>Students</span>
-            </button>
-            <button onClick={()=>setTab('courses')} className={`flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors ${tab == 'courses' ? 'active' : null} `}>
+            </Link>
+          </div>
+          <div>
+            <Link to='/courses' onClick={() => setTab('courses')} className={`flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors ${tab == 'courses' ? 'active' : null} `}>
               <BookOpenText className="w-10 h-10" />
               <span>Courses</span>
-            </button>
-            <button onClick={()=>setTab('instructors')} className={`flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors ${tab == 'instructors' ? 'active' : null} `}>
+            </Link>
+          </div>
+          <div>
+            <Link to='/instructors' onClick={() => setTab('instructors')} className={`flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors ${tab == 'instructors' ? 'active' : null} `}>
               <GraduationCap className="w-20 h-5" />
               <span>Instructors</span>
-            </button>
-            <button onClick={()=>setTab('applications')} className={`flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors ${tab == 'applications' ? 'active' : null} `}>
+            </Link>
+          </div>
+          <div>
+            <Link to='/applications' onClick={() => setTab('applications')} className={`flex items-center gap-2 p-2 rounded-md hover:bg-gray-100 hover:text-blue-600 transition-colors ${tab == 'applications' ? 'active' : null} `}>
               <FileText className="w-20 h-5" />
               <span>Applications</span>
-            </button>
-
-          </nav>
-          <div id='tab'>
-            {tab == 'dashboard' ? <Dashboard /> : null}
-            {tab == 'students' ? <Student /> : null}
+            </Link>
           </div>
-        </>
-        :
-        <Login setLogin={setLogin} />}
+
+        </nav>
+        <Routes>
+          <Route path="/dasboard" element={<Dashboard />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/studentActions/new" element={<NewStudent/>} />
+          
+        </Routes>
+        {/* <div id='tab'>
+          {tab == 'dashboard' ? <Dashboard /> : null}
+          {tab == 'students' ? <Student /> : null}
+        </div> */}
+      </>
     </>
   )
 }
