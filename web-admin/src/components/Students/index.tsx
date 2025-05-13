61691031a657b640498d039223b11f1527c3f971
Hanzala Khan <pythondeveloper360@gmail.com>
2025-05-13 16:13:50 +0500

    feat: implement checkbox selection for students and update styles for actions

--- web-admin/src/components/Students/index.tsx
+++ web-admin/src/components/Students/index.tsx
@@ -5,34 +5,59 @@ import { Link } from 'react-router-dom';
 interface Student {
   id: number,
   last_name: String,
-  first_name:String,
+  first_name: String,
   docs_folder: string
   email: string
 }
 
 const Students: React.FC = () => {
   let [students, setStudents] = useState<Student[]>([])
+  const [selected, setSelected] = useState<Set<number>>(new Set());
+
   const loadStudents = async () => {
     let response = await fetch('http://localhost:5000/api/v1/students')
     let data = await response.json()
     setStudents(data.students)
   }
+  const toggleOne = (id: number) => {
+    const newSet: Set<number> = new Set(selected);
+    newSet.has(id) ? newSet.delete(id) : newSet.add(id);
+    setSelected(newSet);
+  };
+  const toggleAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSet: Set<number> = e.target.checked
+      ? new Set<number>(students.map((student) => student.id))
+      : new Set<number>();
+    setSelected(newSet);
+  };
   useEffect(() => {
     loadStudents()
   }, [])
   return (
     <div>
       <h1>Students</h1>
-      <div id='actions'>
+      <div className='actions'>
         <Link to='/studentActions/new'>+ Add</Link>
         <Link to='/studentActions/attendances'>Attendences</Link>
         <Link to='/studentActions/performance'>Performance</Link>
       </div>
-      <div id = 'tableDiv'>
-
+      <div id='tableDiv'>
+        {selected.size > 0 ? <div className='actions'>
+          <button>Message</button>
+          <button>Attendance Summary</button>
+          <button>Assign</button>
+          <button>Suspend</button>
+        </div> : null}
         <table>
           <thead>
             <tr>
+              <th>
+                <input
+                  type="checkbox"
+                  onChange={toggleAll}
+                  checked={selected.size === students.length}
+                />
+              </th>
               <th>ID</th>
               <th>Name</th>
               <th>Email</th>
@@ -42,6 +67,14 @@ const Students: React.FC = () => {
           <tbody>
             {students.map((student) => (
               <tr key={student.id}>
+                <td className="text-center">
+                  <input
+                    type="checkbox"
+                    checked={selected.has(student.id)}
+                    onChange={() => toggleOne(student.id)}
+                  />
+                </td>
+
                 <td>{student.id}</td>
                 <td>{student.first_name} {student.last_name}</td>
                 <td>{student.email}</td>
