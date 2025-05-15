f77b13044f609a2c176497e2a6285f2c9ee62af8
Hanzala Khan <pythondeveloper360@gmail.com>
2025-05-15 20:22:32 +0500

    Included Applicant table

--- server/app.py
+++ server/app.py
@@ -33,7 +33,7 @@ class Students(db.Model):
             "first_name": self.first_name,
             "last_name": self.last_name,
             "docs_folder": self.docs_folder,
-            "age": self.age,
+            "dob": self.dob,
             "cnic": self.cnic,
             "father_cnic": self.father_cnic,
             "address": self.address,
@@ -55,6 +55,49 @@ class Students(db.Model):
         return f"<Student> {self.id}"
 
 
+class Applicants(db.Model):
+    __tablename__ = "applicants"
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
+    cnic = db.Column(db.String(20))
+    father_cnic = db.Column(db.String(20))
+    docs_folder = db.Column(db.String(20))
+    dob = db.Column(db.DateTime)
+    address = db.Column(db.String(100))
+    phone = db.Column(db.String(15))
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(20))
+    __table_args__ = (UniqueConstraint("email"),)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "docs_folder": self.docs_folder,
+            "dob": self.dob,
+            "cnic": self.cnic,
+            "father_cnic": self.father_cnic,
+            "address": self.address,
+            "phone": self.phone,
+            "email": self.email,
+            "password": self.password,
+        }
+
+    def to_dict_short(self):
+        return {
+            "id": self.id,
+            "last_name": self.last_name,
+            "first_name": self.first_name,
+            "docs_folder": self.docs_folder,
+            "email": self.email,
+        }
+
+    def __repr__(self):
+        return f"<Applicant> {self.id}"
+
+
 class Courses(db.Model):
     __tablename__ = "courses"
     id = db.Column(db.Integer, primary_key=True)
@@ -64,6 +107,7 @@ class Courses(db.Model):
     instructor = db.relationship("Instructors", backref="courses")
     status = db.Column(db.String(20))
     timings = db.Column(db.JSON)
+
     def to_dict_short(self):
         return {
             "id": self.id,
@@ -71,6 +115,7 @@ class Courses(db.Model):
             "status": self.status,
             "timings": self.timings,
         }
+
     def to_dict(self):
         return {
             "id": self.id,
