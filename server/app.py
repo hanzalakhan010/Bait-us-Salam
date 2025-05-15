add142e7724eaa5c5753c4e59ebd67110cbdfabe
Hanzala Khan <pythondeveloper360@gmail.com>
2025-05-15 20:25:03 +0500

    added foreign key in application table

--- server/app.py
+++ server/app.py
@@ -145,6 +145,7 @@ class Applications(db.Model):
     __tablename__ = "applications"
     id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey("students.id"), nullable=False)
+    applicant_id = db.Column(db.Integer, db.ForeignKey("applicants.id"), nullable=True)
     course_id = db.Column(db.Integer, db.ForeignKey("courses.id"), nullable=False)
     application_date = db.Column(db.Date, nullable=True)
     status = db.Column(db.String(20), nullable=True)
@@ -153,6 +154,7 @@ class Applications(db.Model):
     updated_at = db.Column(db.DateTime, nullable=True)
     additional_info = db.Column(db.JSON)
     student = db.relationship("Students", backref="applications")
+    applicant = db.relationship("Applicants", backref="application")
     course = db.relationship("Courses", backref="applications")
 
 
