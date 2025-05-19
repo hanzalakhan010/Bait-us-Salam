6fd3705d29e90cc6c7ea851c71b9234a731771d6
Hanzala Khan <pythondeveloper360@gmail.com>
2025-05-19 07:42:43 +0500

    Refactor student routes: rename functions for clarity and add attendance management endpoint

--- server/app/routes/students.py
+++ server/app/routes/students.py
@@ -2,8 +2,8 @@ from flask import Blueprint, request, jsonify
 from app.models.students import Students
 from app.models import db
 from app.services.students import (
-    editStudentById,
-    getStudentById,
+    editStudentDetailsById,
+    getStudentDetailsById,
     removeStudentById,
     getAllStudents,
     registerStudent,
@@ -12,15 +12,23 @@ from app.services.students import (
 student_blueprint = Blueprint("students", __name__)
 
 
-@student_blueprint.route("/<int:student_id>", methods=["GET", "PATCH", "DELETE"])
-def StudentManagmentById(student_id):
+@student_blueprint.route("/<int:student_id>/attendances", methods=["GET", "POST"])
+def StudentByManagementById(student_id):
+    if request.method == "GET":
+        ...
+
+
+@student_blueprint.route(
+    "/<int:student_id>/details/", methods=["GET", "PATCH", "DELETE"]
+)
+def StudentDetailsManagmentById(student_id):
     # student = Students.query.get_or_404(student_id)
     if request.method == "GET":
-        return jsonify(getStudentById(student_id=student_id)), 200
+        return jsonify(getStudentDetailsById(student_id=student_id)), 200
 
     elif request.method == "PATCH":
         data = request.json
-        editStudentById(student_id=student_id, data=data)
+        editStudentDetailsById(student_id=student_id, data=data)
         return jsonify({"message": "Student updated successfully"}), 200
 
     elif request.method == "DELETE":
