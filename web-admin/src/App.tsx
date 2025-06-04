306d768f5f4da41f12f36e406395c682b8db3fd4
Hanzala Khan <pythondeveloper360@gmail.com>
2025-06-04 07:16:28 +0500

    add authentication check on app load and implement single application route

--- web-admin/src/App.tsx
+++ web-admin/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
@@ -14,10 +14,33 @@ import Instructors from './components/Instructors';
 import NewInstructor from './components/Instructors/newInstructor';
 import Applicaions from './components/Applications';
 import Login from './components/Login';
+import SingleApplication from './components/Applications/singleApplication';
 
 function App() {
   const [tab, setTab] = useState('dashboard')
   const [login, setLogin] = useState(false)
+  const checkAuth = async () => {
+    let email = localStorage.getItem('email')
+    let token = localStorage.getItem('token')
+    console.log(email, token)
+    if (email && token) {
+      let response = await fetch('http://localhost:5000/api/v1/auth/auth', {
+        method: "POST",
+        headers: {
+          'Content-Type': "application/json"
+        },
+        body: JSON.stringify({
+          email, token
+        })
+      })
+      if (response.status == 200) {
+        setLogin(true)
+      }
+    }
+  }
+  useEffect(() => {
+    checkAuth()
+  })
   return (
     <>
 
@@ -72,6 +95,7 @@ function App() {
               <Route path="/instructorActions/new" element={<NewInstructor />} />
               {/* For Applications */}
               <Route path="/applications" element={<Applicaions />} />
+              <Route path='/applications/:id' element={<SingleApplication />} />
             </Routes>
           </>
           ) : (
