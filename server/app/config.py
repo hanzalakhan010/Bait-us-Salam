93c78a736ac1d2002bdf97ca72fae8b94348cd8f
Hanzala Khan <pythondeveloper360@gmail.com>
2025-06-08 23:09:21 +0500

    update SECRET_KEY in configuration for enhanced security

--- server/app/config.py
+++ server/app/config.py
@@ -4,4 +4,4 @@ from uuid import uuid4
 class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:pos-72270@localhost/bwt"
     DEBUG = True
-    SECRET_KEY = str(uuid4())
+    SECRET_KEY = "3c0f64f9-5d65-4f09-98b8-7417be0cc83c"
