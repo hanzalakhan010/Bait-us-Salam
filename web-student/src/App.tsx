6e9bdcfa76c80b6a14ed628d8907cdbc4b677276
Hanzala Khan <pythondeveloper360@gmail.com>
2025-06-22 10:33:38 +0500

    add AvailableCourses and Apply components; update routing and styles

--- web-student/src/App.tsx
+++ web-student/src/App.tsx
@@ -10,6 +10,8 @@ import { notifySuccess } from './notification';
 import Courses from './components/Courses';
 import Applications from './components/Applications';
 import ApplicationView from './components/Applications/ApplicationView';
+import AvailableCourses from './components/AvailableCourses';
+import Apply from './components/AvailableCourses/Apply';
 function App() {
   const [authState, setAuthState] = useState<'authenticated' | 'loading' | 'unauthenticated'>("loading")
   const [navOpen, setNavOpen] = useState(false)
@@ -28,10 +30,11 @@ function App() {
     }
   }
   const logout = async () => {
-    setAuthState('loading')
+    setAuthState('unauthenticated')
+    localStorage.clear()
+    notifySuccess('Logout Successfully')
     try {
-      let response = await fetch(`${host}/api/v1/auth/logout`, { method: 'POST', credentials: 'include' })
-      if (response.ok) { setAuthState('unauthenticated'); notifySuccess('Logout Successfully'); localStorage.clear() }
+      await fetch(`${host}/api/v1/auth/logout`, { method: 'POST', credentials: 'include' })
     } catch {
       alert('Error connecting to server')
     }
@@ -52,6 +55,7 @@ function App() {
                 <Link to='/courses' onClick={() => { setNavOpen(false) }}>Courses</Link>
                 <Link to='/grades' onClick={() => { setNavOpen(false) }}>GradeBook</Link>
                 <Link to='/applications' onClick={() => { setNavOpen(false) }}>Applications</Link>
+                <Link to='/available_courses' onClick={() => { setNavOpen(false) }}>Available Courses</Link>
                 <a onClick={() => { setNavOpen(false); logout() }}>Logout</a>
               </nav>
             ) : <div>
@@ -63,7 +67,8 @@ function App() {
             <Route path="/courses" element={<Courses />} />
             <Route path="/applications" element={<Applications />} />
             <Route path="/applications/:application_id" element={<ApplicationView />} />
-            <Route path="/courses" element={<div></div>} />
+            <Route path="/available_courses" element={<AvailableCourses />} />
+            <Route path='/apply/:course_id' element={<Apply />}></Route>
           </Routes>
         </div >
       )
